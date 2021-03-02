c3b2ba4ac344af2f60e78d7e0b2b7f0014d1042a
YeHwan_Oh <yehwan_oh@tmax.co.kr>
2021-03-02 10:16:04 +0900

    2021-03-02 props-isRequired사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name={3}>칠드런</MyComponent>;
+  return (
+    <MyComponent name="React" favoriteNumber={1}>
+      칠드런
+    </MyComponent>
+  );
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       Props 써보기, {name} <br />
       children 값은 {children}
       입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
@@ -18,6 +20,7 @@ MyComponent.defaultProps = {
 
 MyComponent.propTypes = {
   name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
