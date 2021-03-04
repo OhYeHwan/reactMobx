40fd1c8b7fb84d9ea136b3c60b4ee713e7ef8505
YeHwan_Oh <yehwan_oh@tmax.co.kr>
2021-03-04 10:43:19 +0900

    2021-03-04 8.2.3:useEffect 뒷정리하기

--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 
 const Counter = () => {
+  // useState 함수의 파라미터에는 상태의 기본값을 넣어준다
+  // useState는 함수가 호출되면 배열을 반환 한다.
+  // 첫번째 원소는 상태값, 두번째 원소는 상태를 설정하는 함수
+  // 그렇기 때문에 비구조화 할당을 이용하여 맵핑한다
   const [value, setValue] = useState(0);
 
   return (
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -9,12 +9,13 @@ const Info = () => {
   // useEffect에서 설정한 함수를 컴포넌트가 화면에 맨 처음 렌더링될 때만 실행하고, 업데이트 될 때는 실행하지
   // 않으려면 함수의 두번째 파라미터로 비어있는 배열을 넣어주면 된다 []
   useEffect(() => {
-    console.log("렌더링이 완료되었습니다.");
-    console.log({
-      name,
-      nickname,
-    });
-  }, [name]); //특정 값이 업데이트 될 때만 실행하고 싶을 때는 두번째 배열안에 검사하고 싶은 값을 넣어주면 된다.
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  }, []); //특정 값이 업데이트 될 때만 실행하고 싶을 때는 두번째 배열안에 검사하고 싶은 값을 넣어주면 된다.
 
   const onChangeName = (e) => {
     setName(e.target.value);
