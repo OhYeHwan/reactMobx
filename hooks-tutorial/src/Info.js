1d6e22f403663308a49f971cbd032e16c04328e5
YeHwan_Oh <yehwan_oh@tmax.co.kr>
2021-03-04 10:32:10 +0900

    2021-03-04 03.useEffect:특정 값이 업데이트 될때만 호출하기

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -5,13 +5,16 @@ const Info = () => {
   const [nickname, setNickname] = useState("");
 
   //useEffect는 리액트 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hooks이다.
+
+  // useEffect에서 설정한 함수를 컴포넌트가 화면에 맨 처음 렌더링될 때만 실행하고, 업데이트 될 때는 실행하지
+  // 않으려면 함수의 두번째 파라미터로 비어있는 배열을 넣어주면 된다 []
   useEffect(() => {
     console.log("렌더링이 완료되었습니다.");
     console.log({
       name,
       nickname,
     });
-  });
+  }, [name]); //특정 값이 업데이트 될 때만 실행하고 싶을 때는 두번째 배열안에 검사하고 싶은 값을 넣어주면 된다.
 
   const onChangeName = (e) => {
     setName(e.target.value);
