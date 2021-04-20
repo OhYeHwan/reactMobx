f91cd84912156ba7424e33247cdfbfb6ceeb5b71
OOHYEE <azss0470@gmail.com>
2021-04-20 23:27:50 +0900

    210420 - 14.6.2 API 호출할때 카테고리 지정하기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,26 +1,18 @@
-import React, { useState } from "react";
-import axios from 'axios'
+import React, { useState, useCallback } from "react";
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const reponse = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=b5f1ee6a55ea4908a0b987cfbbb180b9',
-      );
-      setData(reponse.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category),[]);
 
   return (
     <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true}/>}
+      <Categories category={category} onSelect={onSelect}/>
+      <NewsList category={category}/>;
     </div>
-  );
+  )
 };
+
 export default App;
