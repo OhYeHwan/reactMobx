42e7ef5fde7d04a9e6b45a1cdca611ba0ba87dc3
OOHYEE <azss0470@gmail.com>
2021-04-20 21:33:13 +0900

    210420 - News API 받아오기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,10 +3,15 @@ import axios from 'axios'
 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
-      setData(response.data);
-    });
+  const onClick = async () => {
+    try {
+      const reponse = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=b5f1ee6a55ea4908a0b987cfbbb180b9',
+      );
+      setData(reponse.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
